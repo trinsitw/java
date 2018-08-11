4ef3334a6404f4a4e606cac393b856e9d14f7e3c
Trinsit Wasinudomrod <trinsit.w@gmail.com>
2018-08-11 12:55:30 +0700

    New cycle detection algorithm.

--- GraphTheory/src/main/java/com/intellectworld/graphtheory/algorithm/Example.java
+++ GraphTheory/src/main/java/com/intellectworld/graphtheory/algorithm/Example.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 public class Example {
 
     public static void main(String[] args) {
-    /*    {
+        {
             Vertex v1 = new Vertex("v1");
             Vertex v2 = new Vertex("v2");
             Vertex v3 = new Vertex("v3");
@@ -40,27 +40,28 @@ public class Example {
         System.out.println("------------------------------------------------");
 
         {
-            Vertex v1 = new Vertex("v1");
-            Vertex v2 = new Vertex("v2");
-            Vertex v3 = new Vertex("v3");
-            Vertex v4 = new Vertex("v4");
-            Vertex v5 = new Vertex("v5");
-            Vertex v6 = new Vertex("v6");
-            Set<Vertex> vertices = Stream.of(v1, v2, v3, v4, v5, v6).collect(Collectors.toSet());
-            WeightedDirectedEdge e12 = new WeightedDirectedEdge("e12", v1, v2, 1);
-            WeightedDirectedEdge e13 = new WeightedDirectedEdge("e13", v1, v3, 1);
-            WeightedDirectedEdge e23 = new WeightedDirectedEdge("e23", v2, v3, 1);
-            WeightedDirectedEdge e41 = new WeightedDirectedEdge("e41", v4, v1, 1);
-            WeightedDirectedEdge e45 = new WeightedDirectedEdge("e45", v4, v5, -1);
-            WeightedDirectedEdge e56 = new WeightedDirectedEdge("e56", v5, v6, -1);
-            WeightedDirectedEdge e64 = new WeightedDirectedEdge("e64", v6, v4, -1);
-            Set<WeightedDirectedEdge> edges = Stream.of(e12, e13, e23, e41, e45, e56, e64).collect(Collectors.toSet());
+            Vertex A = new Vertex("A");
+            Vertex B = new Vertex("B");
+            Vertex C = new Vertex("C");
+            Vertex D = new Vertex("D");
+            Vertex E = new Vertex("E");
+            Set<Vertex> vertices = Stream.of(A, B, C, D, E).collect(Collectors.toSet());
 
-            WeightedDirectedGraph inputGraph = new WeightedDirectedGraph(vertices, edges);
-            System.out.println("inputGraph: " + inputGraph);
-            NegativeCycleDetectionAlgorithm negativeCycleDetectionAlgorithm = new DfsAlgorithm();
-            WeightedDirectedGraph negativeCycle = negativeCycleDetectionAlgorithm.findNegativeCycle(inputGraph);
-            System.out.println("negativeCycle: " + negativeCycle);
-        }*/
+            WeightedDirectedEdge AB = new WeightedDirectedEdge("AB", A, B, 1);
+            WeightedDirectedEdge BC = new WeightedDirectedEdge("BC", B, C, 1);
+            WeightedDirectedEdge CD = new WeightedDirectedEdge("CD", C, D, 1);
+            WeightedDirectedEdge DE = new WeightedDirectedEdge("DE", D, E, 1);
+            WeightedDirectedEdge EA = new WeightedDirectedEdge("EA", E, A, 1);
+            WeightedDirectedEdge DA = new WeightedDirectedEdge("DA", D, A, 1);
+            WeightedDirectedEdge CA = new WeightedDirectedEdge("CA", C, A, 1);
+            Set<WeightedDirectedEdge> edges = Stream.of(AB, BC, CD, DE, EA, DA, CA).collect(Collectors.toSet());
+
+            WeightedDirectedGraph graph = new WeightedDirectedGraph(vertices, edges);
+            System.out.println("graph: " + graph);
+            CycleDetectionAlgorithm cycleDetectionAlgorithm = new DPTAlgorithm();
+            Set<List<WeightedDirectedEdge>> cycles = cycleDetectionAlgorithm.findCycles(graph, A);
+            System.out.println("cycles: " + cycles);
+
+        }
     }
 }
